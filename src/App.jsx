ecce4be5bb13551ac06a1a79b8be84b4d81c6950
nishaj0 <njnishaj0@gmail.com>
2023-10-06 19:48:16 +0530

    completed 3-post-req

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Title from './components/Title';
-import Setup from './examples/2-headers'
+import Setup from './examples/3-post-request'
 function App() {
   return (
     <main>
